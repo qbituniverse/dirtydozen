a0c4c70d4b01e35bfb0061cb6ee62c5f4480e0b9
qbituniverse <48298870+qbituniverse@users.noreply.github.com>
2024-09-04 22:25:04 +0200

    Refactor (#14)
    Co-authored-by: Marcin Narloch <marcin.narloch@qbituniverse.com>

--- .cicd/setup.sh
+++ .cicd/setup.sh
@@ -5,7 +5,7 @@
 docker compose -f .cicd/docker-compose.Development.yaml up
 
 # browse
-start http://localhost:4000
+http://localhost:4000
 
 # take down
 docker compose -f .cicd/docker-compose.Development.yaml down
